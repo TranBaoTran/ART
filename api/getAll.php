4802762ebe04378f1047740f38501b8a124339ec
TranBaoTran <117694181+TranBaoTran@users.noreply.github.com>
2023-05-02 13:03:09 +0700

    feat: product paging

--- api/getAll.php
+++ api/getAll.php
@@ -1,14 +1,39 @@
 <?php 
     include_once "../database.php";
+    include_once "../pagination.php";
 
     $db=new Database();
 
     $limit=isset( $_GET['limit'] ) ? $_GET['limit'] : 9;
-    $offset=isset( $_GET['offset'] ) ? $_GET['offset'] : 0;
+    $page=isset( $_GET['page'] ) ? $_GET['page'] : 1;
+    $offset=($page-1)*9;
+    // $offset=isset( $_GET['offset'] ) ? $_GET['offset'] : 0;
+    
+    if(isset($_GET['gen'])){
+        if(isset($_GET['category'])){
+            $member = $db->Table('sanpham')->Limit($limit)->Offset($offset)->GetCate($_GET['category']);
+            $total=$db->Table('sanpham')->ID($_GET['category'])->CountCate('malsp');
+        }
+        else{
+            $member = $db->Table('sanpham')->Limit($limit)->Offset($offset)->GetGen($_GET['gen']);
+            $total=$db->Table('sanpham')->ID($_GET['gen'])->CountGen('theloai','malsp','malsp','macl');
+        }
+    }
+    else{
+        $member = $db->Table('sanpham')->Limit($limit)->Offset($offset)->Get();
+        $total=$db->Table('sanpham')->CountAll();
+    } 
+  
+    $result=new stdClass();
+    $result->member=$member;
+    $result->limit= (int)$limit;
+    $result->current_page =  (int)$page;
+    $result->allPage =  ceil($total/$limit);
 
-    $member = $db->Table('sanpham')->Limit( $limit)->Offset($offset)->Get();
+
+    
     $db->Close();
+    echo json_encode($result);
 
-    echo json_encode($member);
-
+    
 ?>
--- product.php
+++ product.php
@@ -1,151 +1,96 @@
-<?php
-    require_once "database.php";
-    include_once "pagination.php";
-
-    $db=new Database();
-
-    $LFull='';
-    $LFirst='';
-    $total;
-
-    $cate='';
-    $gen='';
-    $page='';
-
-    if(isset($_GET['gen'])){
-        if(isset($_GET['category'])){
-            $LFull="index.php?id=product&gen=".$_GET['gen']."&category=".$_GET['category']."&page={page}";
-            $LFirst="index.php?id=product&gen=".$_GET['gen']."&category=".$_GET['category'];
-            $total=$db->Table('sanpham')->ID($_GET['category'])->CountCate('malsp');
-            $gen.="&gen=".$_GET['gen'];
-            $cate.="&category=".$_GET['category'];
-        }
-        else{
-            $LFull="index.php?id=product&gen=".$_GET['gen']."&page={page}";
-            $LFirst="index.php?id=product&gen=".$_GET['gen'];
-            $total=$db->Table('sanpham')->ID($_GET['gen'])->CountGen('theloai','malsp','malsp','macl');
-            $gen.="&gen=".$_GET['gen'];
-        }
-    }
-    else{
-        $LFull="index.php?id=product&page={page}";
-        $LFirst="index.php?id=product";
-        $total=$db->Table('sanpham')->CountAll();
-    }
-
-    if(isset($_GET['page'])){
-        $page="&page=".$_GET['page'];
-    }
-    else{
-        $page="&page=1";
-    }
-
-    $config = array(
-        'current_page'  => isset($_GET['page']) ? $_GET['page'] : 1,
-        'total_record'  => $total, // tổng số thành viên
-        'limit'         => 9,
-        'link_full'     => $LFull,
-        'link_first'    => $LFirst,
-        'range'         => 5
-    );
-
-    $paging = new Pagination();
-    $paging->init($config);
- 
-    $limit = $paging->get_config('limit');
-    $start = $paging->get_config('start');
-    
-    if(isset($_GET['gen'])){
-        if(isset($_GET['category'])){
-            $member = $db->Table('sanpham')->Limit($limit)->Offset($start)->GetCate($_GET['category']);
-        }
-        else{
-            $member = $db->Table('sanpham')->Limit($limit)->Offset($start)->GetGen($_GET['gen']);
-        }
-    }
-    else{
-        $member = $db->Table('sanpham')->Limit($limit)->Offset($start)->Get();
-    } 
-
-    $db->close();
-
-    if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-        die (json_encode(array(
-            'member' => $member,
-            'paging' => $paging->html()
-        )));
-    } 
-?>
-
-<div class="banner">
-        <h3 class="banner_content">Sản phẩm</h3>
-</div>
-<div id="MENU_CONTAIN">
+    <div class="banner">
+    <h3 class="banner_content">Sản phẩm</h3>
+    </div>
+    <div id="MENU_CONTAIN">
     <div class="MENU_SP" style="display: flex;" id="productWraper">
-        <?php foreach ($member as $item){ 
-            $URL="id=product".$gen.$cate.$page;
-            echo "<div class='SP_CON shadow'>
-            <div class='SP_CON1'>
-                <img src='".$item->img."'>
-            </div>
-            <div class='SP_CON2'>
-                <div>
-                    <h3>".$item->tensp."</h3><br>
-                    <a class='price'>".$item->gia." VNĐ</a><br>
-                </div>
-                <div style='padding-top: 20px;'>
-                    <a href='index.php?".$URL."&pr=".$item->masp."&click=clicked' class='btn'>Thêm vào giỏ</a>
-                </div>
-            </div>
-            </div>";
-        }    
-        ?>
+        
     </div>
 
     <div style="width:100%;display: flex;justify-content: center;" id="pageWraper">
-        <?php echo $paging->html(); ?>
+        <a href="#"><div class="page_num active">1</div></a> 
     </div>
-</div>
-
-<?php 
-    if (isset($_GET['click'])) {
-        include "single.php";
+    </div>
+<script>
+    function createParameter(value){
+        if(value != null)  return `"${value}"`;
+        return null;
     }
-?>
 
-<!-- <script>
-    $('#MENU_CONTAIN').on('click','#pageWraper a', function ()
-             {
-                 var url = $(this).attr('href');
-                 alert(url);
-                 $.ajax({
-                     url : url,
-                     type : "GET",
-                     dataType : 'json',
-                     success : function (result)
-                     {
-                         var html;
-                         if (result.hasOwnProperty('member') && result.hasOwnProperty('paging'))
-                         {
-                             $.each(result['member'], function (key, item){
-                                html += "<div class='SP_CON shadow'><div class='SP_CON1'><img src='"+$item['img']+"></div>";
-                                html += "<div class='SP_CON2'><div><h3>"+$item['tensp']+"</h3><br><a class='price'>"+$item['gia']+" VNĐ</a><br></div>";
-                                html += "<div style='padding-top: 20px;'><a class='btn'>Thêm vào giỏ</a></div></div></div>";
-                             });
-                              
-                             $('#productWraper').html(html);s
-                              
-                             $('#pageWraper').html(result['paging']);
-                              
-                             window.history.pushState({path:url},'',url);
-                         }
-                     },
-                    error: function(xhr, ajaxOptions, thrownError) {
+    function renderPagination(currentPage,allPage,limit,gen,cate){
+        let html="";
+        let start=1;
+        let end=5;
+        if(currentPage-2>0){
+            start=currentPage-2;
+            end=currentPage+2;
+        }
+        if(currentPage+2>allPage){
+            start=allPage-4;
+            end=allPage;
+        }
+        if(allPage<=5){
+            start=1;
+            end=allPage;
+        }
+        if(currentPage>1){
+            html+="<a href='#/'><div class='page_num' onclick='getPro(1,"+limit+","+createParameter(gen)+","+createParameter(cate)+")'><i class='fa-solid fa-angles-left'></i></div></a> ";
+            html+="<a href='#/'><div class='page_num' onclick='getPro("+(currentPage-1)+","+limit+","+createParameter(gen)+","+createParameter(cate)+")'><i class='fa-solid fa-angle-left'></i></div></a> ";
+        }
+        for(let i=start;i<=end;i++){
+            html+="<a href='#/'><div class='page_num";
+            if(i==currentPage){
+                html+=" active'>"+i+"</div></a> ";
+            }
+            else{
+                html+="' onclick='getPro("+i+","+limit+","+createParameter(gen)+","+createParameter(cate)+")'>"+i+"</div></a> ";
+            }
+        }
+        if(currentPage<allPage){
+            html+="<a href='#/'><div class='page_num' onclick='getPro("+(currentPage+1)+","+limit+","+createParameter(gen)+","+createParameter(cate)+")'><i class='fa-solid fa-angle-right'></i></div></a> ";
+            html+="<a href='#/'><div class='page_num' onclick='getPro("+allPage+","+limit+","+createParameter(gen)+","+createParameter(cate)+")'><i class='fa-solid fa-angles-right'></i></div></a> ";
+        }
+        $("#pageWraper").html(html); 
+        let url=window.location.href.split('?')[0];
+        url+="?id=product&page="+currentPage+"&limit="+limit;
+        if(gen){
+            url+="&gen="+gen;
+            if(cate){
+                url+="&category="+cate;
+            }
+        }
+        window.history.pushState({},'',url);
+    }
+
+    function getPro(page=1,limit=9,gen,cate){
+        let url="/ART/api/getAll.php?limit="+limit+"&page="+page;
+        if(gen){
+            url+="&gen="+gen;
+            if(cate){
+                url+="&category="+cate;
+            }
+        }
+        const pr=$.ajax({
+            url:url,
+            type: "GET",
+            dataType: 'json',
+            success : function (result){
+                var html="";
+                $.each(result['member'], function (key, item){
+                                html += "<div class='SP_CON shadow'><div class='SP_CON1'><img src='"+item['img']+"'></div>";
+                                html += "<div class='SP_CON2'><div><h3>"+item['tensp']+"</h3><br><a class='price'>"+item['gia']+" VNĐ</a><br></div>";
+                                html += "<div style='padding-top: 20px;'><a class='btn' >Thêm vào giỏ</a></div></div></div>";
+                             });             
+                $('#productWraper').html(html);
+                renderPagination(result.current_page,result.allPage,result.limit,gen,cate);           
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
                         console.log(thrownError);
-                        alert('Lỗi sml r đừng cố')
-                    },
-                 });
-                 return false;
-             });
-</script> -->
+                        alert('Lỗi sml r đừng cố');
+            },
+        });
+    }
 
+    const queryString = window.location.search;
+    const urlParam = new URLSearchParams(queryString);
+    getPro(urlParam.get('page') || 1,urlParam.get('limit')|| 9,urlParam.get('gen'),urlParam.get('category'));
+</script>
